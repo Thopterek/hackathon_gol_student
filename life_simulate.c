b6e02ac776079540de14ee065c2aa00c3ebc6dc6
Nataniel Dziadzia <nataniel.dziadzia@gmail.com>
2025-09-30 16:56:56 +0200

    closer or not we will see

--- life_simulate.c
+++ life_simulate.c
@@ -69,62 +69,101 @@ typedef struct
   y 0 x 0, index 0;
   y 0 x 1, index 1;
 */
+
+uint32_t counting_cells(uint8_t *grid, uint32_t cc, uint32_t whole_grid) {
+  uint32_t counter = 0;
+  if (cc > 3 && cc < whole_grid - 4) {
+    uint32_t tmp = cc - 4;
+    while (tmp < cc) {
+    if (grid[tmp] == 1)
+      counter++;
+    tmp++;
+    }
+    tmp = cc + 1;
+    while (tmp < cc + 4) {
+      if (grid[tmp] == 1)
+        counter++;
+      tmp++;
+    }
+  }
+  else if (cc <= 3) {
+    uint32_t tmp = whole_grid - (4 - cc);
+    while (tmp < whole_grid) {
+      if (grid[tmp] == 1)
+        counter++;
+      tmp++;
+    }
+    tmp = 0;
+    while (tmp < cc) {
+      if (grid[tmp] == 1)
+        counter++;
+      tmp++;
+    }
+    tmp = cc + 1;
+    while (tmp < cc + 4) {
+      if (grid[tmp] == 1)
+        counter++;
+      tmp++;
+    }
+  }
+  else {
+    uint32_t tmp = cc - 4;
+    while (tmp < cc) {
+    if (grid[tmp] == 1)
+      counter++;
+    tmp++;
+    }
+    if (cc < whole_grid - 1) {
+      tmp = cc + 1;
+    }
+    uint8_t checked = 0;
+    while (tmp < whole_grid) {
+      if (grid[tmp] == 1)
+        counter++;
+      checked++;
+      tmp++;
+    }
+    tmp = 0;
+    while (checked < 4) {
+      if (grid[tmp] == 1)
+        counter++;
+      tmp++;
+      checked++;
+    }
+  }
+  return (counter);
+}
+
 uint8_t *simulate_life(uint32_t grid_dim, start_coord_t *initial_points, uint32_t initial_point_count)
 {
   const uint32_t whole_grid = grid_dim * grid_dim;
-  uint8_t *grid = VirtualAlloc(NULL, whole_grid * sizeof(uint8_t), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+  // static int first = 0;
+  uint8_t *grid = NULL;
+  if (grid == NULL) {
+    grid = VirtualAlloc(NULL, whole_grid * sizeof(uint8_t), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+  }
   uint32_t cc = 0;
   while (cc < initial_point_count) {
     grid[initial_points[cc].y * grid_dim + initial_points[cc].x] = 1;
     cc++;
   }
   cc = 0;
-  uint8_t to_keep[256] = {0};
+  uint8_t to_keep[1024] = {0};
   while (cc < whole_grid) {
     /*
       Checking the living cells
     */
     if (grid[cc] == 1) {
-      uint32_t counter = 0;
-      if (cc > 3 && cc < whole_grid - 4) {
-        uint32_t tmp = cc - 4;
-        while (tmp < cc) {
-          if (grid[tmp] == 1)
-            counter++;
-          tmp++;
-        }
-        tmp = cc + 1;
-        while (tmp < cc + 4) {
-          if (grid[tmp] == 1)
-            counter++;
-          tmp++;
-        }
-        if (counter >= 2 && counter <= 3) {
-          to_keep[cc] = 1;
-        }
-      }
+      uint32_t counter = counting_cells(grid, cc, whole_grid); 
+      if (counter == 2 || counter == 3) 
+        to_keep[cc] = 1;
     }
     /*
       Checking the dead ones
     */
     else {
-      uint32_t another_c = 0;
-      if (cc > 3 && cc < whole_grid - 4) {
-        uint32_t ano_tmp = cc - 4;
-        while (ano_tmp < cc) {
-          if (grid[ano_tmp] == 1)
-            another_c++;
-          ano_tmp++;
-        }
-        ano_tmp = cc + 1;
-        while (ano_tmp < cc + 4) {
-          if (grid[ano_tmp] == 1)
-            another_c++;
-          ano_tmp++;
-        }
-        if (another_c == 3)
-          to_keep[cc] = 1;
-      }
+      if (counting_cells(grid, cc, whole_grid) == 3)
+        to_keep[cc] = 1;
     }
     cc++;
   }
@@ -133,6 +172,7 @@ uint8_t *simulate_life(uint32_t grid_dim, start_coord_t *initial_points, uint32_
     grid[cc] = to_keep[cc];
     cc++;
   }
+  return (grid);
   // while (cc < initial_point_count) {
   //   uint32_t inner = 0;
   //   uint32_t temp_x = initial_points[cc].x;
@@ -166,6 +206,4 @@ uint8_t *simulate_life(uint32_t grid_dim, start_coord_t *initial_points, uint32_
   //   }
   //   h++;
   // }
-  return grid;
 }
-
